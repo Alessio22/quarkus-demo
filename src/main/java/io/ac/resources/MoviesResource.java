a171b417c8de33a06eb5ecded67bde5618177381
Alessio Canestrelli <a.canestrelli@gmail.com>
2022-11-16 14:05:32 +0100

    get principal from jwt

--- src/main/java/io/ac/resources/MoviesResource.java
+++ src/main/java/io/ac/resources/MoviesResource.java
@@ -3,17 +3,25 @@ package io.ac.resources;
 import io.ac.dtos.CreateMovieDTO;
 import io.ac.entities.MovieEntity;
 import io.ac.services.MovieService;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.jboss.logging.Logger;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 
 @Path("movies")
 @Tag(name = "Movie resource", description = "A simple resource")
 public class MoviesResource {
+    private Logger LOGGER = Logger.getLogger(MoviesResource.class);
+
+    @Inject
+    JsonWebToken jwt;
 
     @Inject
     MovieService movieService;
@@ -22,8 +30,9 @@ public class MoviesResource {
     @RolesAllowed({"ADMIN", "USER"})
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAll(
-            @QueryParam("directorId") Long directorId
+            @QueryParam("directorId") Long directorId, @Context SecurityContext ctx
     ) {
+        LOGGER.info(getResponseString(ctx));
         return Response
                 .ok(
                         movieService.findAll(directorId).stream().map(MovieEntity::toDto)
@@ -36,8 +45,9 @@ public class MoviesResource {
     @Path("{movieId}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getOne(
-            @PathParam("movieId") Long movieId
+            @PathParam("movieId") Long movieId, @Context SecurityContext ctx
     ) {
+        LOGGER.info(getResponseString(ctx));
         return Response.ok(movieService.findById(movieId).toDto()).build();
     }
 
@@ -45,7 +55,8 @@ public class MoviesResource {
     @RolesAllowed({"ADMIN"})
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response create(CreateMovieDTO createMovieDTO) {
+    public Response create(CreateMovieDTO createMovieDTO, @Context SecurityContext ctx) {
+        LOGGER.info(getResponseString(ctx));
         return Response
                 .ok(
                         movieService.create(createMovieDTO.getTitle(), createMovieDTO.getDirectorId()).toDto()
@@ -60,8 +71,10 @@ public class MoviesResource {
     @Consumes(MediaType.APPLICATION_JSON)
     public Response delete(
             @PathParam("movieId") Long movieId,
-            CreateMovieDTO createMovieDTO
+            CreateMovieDTO createMovieDTO,
+            @Context SecurityContext ctx
     ) {
+        LOGGER.info(getResponseString(ctx));
         return Response.ok(
                 movieService.update(movieId, createMovieDTO.getTitle(), createMovieDTO.getDirectorId()).toDto()
         ).build();
@@ -72,9 +85,30 @@ public class MoviesResource {
     @Path("{movieId}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response delete(
-            @PathParam("movieId") Long movieId
+            @PathParam("movieId") Long movieId, @Context SecurityContext ctx
     ) {
+        LOGGER.info(getResponseString(ctx));
         movieService.delete(movieId);
         return Response.noContent().build();
+    }
+
+    private String getResponseString(SecurityContext ctx) {
+        String name;
+        if (ctx.getUserPrincipal() == null) {
+            name = "anonymous";
+        } else if (!ctx.getUserPrincipal().getName().equals(jwt.getName())) {
+            throw new InternalServerErrorException("Principal and JsonWebToken names do not match");
+        } else {
+            name = ctx.getUserPrincipal().getName();
+        }
+        return String.format("subject: %s,"
+                        + " isHttps: %s,"
+                        + " authScheme: %s,"
+                        + " hasJWT: %s",
+                name, ctx.isSecure(), ctx.getAuthenticationScheme(), hasJwt());
+    }
+
+    private boolean hasJwt() {
+        return jwt.getClaimNames() != null;
     }
 }
